c59d3c9bc6cc8dbf907574abf1bfaa514b346047
SANA BEN <sana.bengannoune@gmail.com>
2020-04-10 01:04:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Séléctionner les aiguilles de montre
-const AiguilleHr = document.querySelector("#hour");
-const AiguilleMin = document.querySelector("#minute");
-const AiguilleSec = document.querySelector("#second");
+const aiguilleHr = document.querySelector("#hour");
+const aiguilleMin = document.querySelector("#minute");
+const aiguilleSec = document.querySelector("#second");
 
 //Extraire l'heure actuel à l'aide de l'objet Date()
 let date = new Date();
@@ -26,9 +26,9 @@ function demarrerLaMontre() {
     minutDeg = minutDeg +(6/60);
     secondDeg = secondDeg+6;
       
-    AiguilleHr.style.transform ="rotate(" + hourDeg +"deg)";
-    AiguilleMin.style.transform ="rotate(" + minutDeg +"deg)";
-    AiguilleSec.style.transform ="rotate(" + secondDeg +"deg)";
+    aiguilleHr.style.transform ="rotate(" + hourDeg +"deg)";
+    aiguilleMin.style.transform ="rotate(" + minutDeg +"deg)";
+    aiguilleSec.style.transform ="rotate(" + secondDeg +"deg)";
 
 }
 // Exercuter la fonction chaque second
